4fc1712d803881760c125f5332b58be80a3a310b
angeliquejoyfernandez <angeliquejoy.fernandez@sjsu.edu>
2025-03-06 21:35:36 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,17 +2,19 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     let fortunes = [
-        "You will have a lucky day!",
-        "An exciting opportunity is coming your way.",
-        "A surprise gift is in your future.",
-        "Something unexpected will bring you joy.",
-        "Your dreams are closer than you think.",
-        "You will receive good news very soon!",
-        "A great adventure awaits you!",
-        "The love of your life will appear in front of you unexpectedly!",
-        "You have a secret admirer.",
-        "The love of your life is right in front of your eyes.",
-        "Do what you love. The rest will fall into place.",
+        // "You will have a lucky day!",
+        // "An exciting opportunity is coming your way.",
+        // "A surprise gift is in your future.",
+        // "Something unexpected will bring you joy.",
+        // "Your dreams are closer than you think.",
+        // "You will receive good news very soon!",
+        // "A great adventure awaits you!",
+        // "The love of your life will appear in front of you unexpectedly!",
+        // "You have a secret admirer.",
+        // "The love of your life is right in front of your eyes.",
+        // "Do what you love. The rest will fall into place.",
+        "angie is your girlfriend",
+        
 
     ];
 
